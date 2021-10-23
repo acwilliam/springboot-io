68148f8bfcd7af15d8d65cfd07a28549ab720524
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2021-10-23 11:29:20 -0300

    configurando conexao com banco

--- src/main/java/com/acwilliam/springboot/dominio/Anime.java
+++ src/main/java/com/acwilliam/springboot/dominio/Anime.java
@@ -2,11 +2,24 @@ package com.acwilliam.springboot.dominio;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Data//cria o get/set/equals/hashcod/tostring
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     private String nome;
 
 }
