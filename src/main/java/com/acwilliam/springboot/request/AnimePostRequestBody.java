9d1fcdc47c37889f35d6efc058f0199655509778
William Carvalho <williamcoelho.noorden@sulamerica.com.br>
2021-10-23 18:50:36 -0300

    configurando requestBody

---
+++ src/main/java/com/acwilliam/springboot/request/AnimePostRequestBody.java
@@ -0,0 +1,8 @@
+package com.acwilliam.springboot.request;
+
+import lombok.Data;
+
+@Data
+public class AnimeRequest {
+    private String nome;
+}
---
+++ src/main/java/com/acwilliam/springboot/request/AnimePutRequestBody.java
@@ -0,0 +1,8 @@
+package com.acwilliam.springboot.request;
+
+import lombok.Data;
+
+@Data
+public class AnimePostRequestBody {
+    private String nome;
+}
